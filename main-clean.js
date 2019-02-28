f1ab895e74f0c80fefe2be0c531f3d0bf3a0ce49
Victor Manuel Herrera Reverter <victormanuel.cuba@gmail.com>
2019-02-28 06:39:19 +0100

    Upload Logic Updated and Working

--- main-clean.js
+++ main-clean.js
@@ -88,13 +88,309 @@ var madLibsOneStr = 'madLibsOne';
 
 // End of MadLibsOne --------------------------------------------------------------
 
+// Begin of MadLibsTwo --------------------------------------------------------------
+
+// MadLibsOne Object 2
+
+var madLibsTwoVals = {
+    madLibsTwo1: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo2: {
+        title: 'Number',
+        value: ''
+    },
+    madLibsTwo3: {
+        title: 'Color',
+        value: ''
+    },
+    madLibsTwo4: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo5: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo6: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo7: {
+        title: 'Number',
+        value: ''
+    },
+    madLibsTwo8: {
+        title: 'Number',
+        value: ''
+    },
+    madLibsTwo9: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo10: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo11: {
+        title: 'Object',
+        value: ''
+    },
+    madLibsTwo12: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo13: {
+        title: 'Color',
+        value: ''
+    },
+    madLibsTwo14: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsTwo15: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo16: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo17: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsTwo18: {
+        title: 'Verb',
+        value: ''
+    }
+}
+
+// Deconstruction of the Object Properties MadLibs 2
+
+var {
+    madLibsTwo1,
+    madLibsTwo2,
+    madLibsTwo3,
+    madLibsTwo4,
+    madLibsTwo5,
+    madLibsTwo6,
+    madLibsTwo7,
+    madLibsTwo8,
+    madLibsTwo9,
+    madLibsTwo10,
+    madLibsTwo11,
+    madLibsTwo12,
+    madLibsTwo13,
+    madLibsTwo14,
+    madLibsTwo15,
+    madLibsTwo16,
+    madLibsTwo17,
+    madLibsTwo18
+} = madLibsTwoVals;
+
+// Base of the Mad Libs Script
+
+var madLibsTwoTitle = 'Jedi Training';
+var madLibsTwo;
+var madLibsTwoStr = 'madLibsTwo';
+
+// End of MadLibsTwo --------------------------------------------------------------
+
+// Begin of MadLibsThree --------------------------------------------------------------
+
+// MadLibsOne Object 3
+
+var madLibsThreeVals = {
+    madLibsThree1: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree2: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree3: {
+        title: 'Noun',
+        value: ''
+    },
+    madLibsThree4: {
+        title: 'Noun',
+        value: ''
+    },
+    madLibsThree5: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree6: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree7: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree8: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsThree9: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree10: {
+        title: 'Body Part',
+        value: ''
+    },
+    madLibsThree11: {
+        title: 'Body Part',
+        value: ''
+    },
+    madLibsThree12: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree13: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsThree14: {
+        title: 'Object',
+        value: ''
+    }
+}
+
+// Deconstruction of the Object Properties MadLibs 3
+
+var {
+    madLibsThree1,
+    madLibsThree2,
+    madLibsThree3,
+    madLibsThree4,
+    madLibsThree5,
+    madLibsThree6,
+    madLibsThree7,
+    madLibsThree8,
+    madLibsThree9,
+    madLibsThree10,
+    madLibsThree11,
+    madLibsThree12,
+    madLibsThree13,
+    madLibsThree14
+} = madLibsThreeVals;
+
+// Base of the Mad Libs Script
+
+var madLibsThreeTitle = 'The Mighty Force';
+var madLibsThree;
+var madLibsThreeStr = 'madLibsThree';
+
+// End of MadLibsThree --------------------------------------------------------------
+
+// Begin of MadLibsFour --------------------------------------------------------------
+
+// MadLibsOne Object 4
+
+var madLibsFourVals = {
+    madLibsFour1: {
+        title: 'Object',
+        value: ''
+    },
+    madLibsFour2: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsFour3: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsFour4: {
+        title: 'Number',
+        value: ''
+    },
+    madLibsFour5: {
+        title: 'Color',
+        value: ''
+    },
+    madLibsFour6: {
+        title: 'Object',
+        value: ''
+    },
+    madLibsFour7: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsFour8: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsFour9: {
+        title: 'Object',
+        value: ''
+    },
+    madLibsFour10: {
+        title: 'Adjective',
+        value: ''
+    },
+    madLibsFour11: {
+        title: 'Verb',
+        value: ''
+    },
+    madLibsFour12: {
+        title: 'Adjective',
+        value: ''
+    }
+}
+
+// Deconstruction of the Object Properties MadLibs 4
+
+var {
+    madLibsFour1,
+    madLibsFour2,
+    madLibsFour3,
+    madLibsFour4,
+    madLibsFour5,
+    madLibsFour6,
+    madLibsFour7,
+    madLibsFour8,
+    madLibsFour9,
+    madLibsFour10,
+    madLibsFour11,
+    madLibsFour12
+} = madLibsFourVals;
+
+// Base of the Mad Libs Script
+
+var madLibsFourTitle = 'Rebel Princess';
+var madLibsFour;
+var madLibsFourStr = 'madLibsFour';
+
+// End of MadLibsFour --------------------------------------------------------------
+
+// Execution Alternatively the MadLibs Filler
+
 var randomMadLibs = Math.floor(Math.random() * 4) + 1;
 console.log(randomMadLibs);
 
-madLibsExec(madLibsOne, madLibsOneStr, madLibsOneVals, madLibsOneTitle, 1);
+// madLibsExec(madLibsOne, madLibsOneStr, madLibsOneVals, madLibsOneTitle, 1);
+// madLibsExec(madLibsTwo, madLibsTwoStr, madLibsTwoVals, madLibsTwoTitle, 2);
+// madLibsExec(madLibsThree, madLibsThreeStr, madLibsThreeVals, madLibsThreeTitle, 3);
+// madLibsExec(madLibsFour, madLibsFourStr, madLibsFourVals, madLibsFourTitle, 4);
+
+if (randomMadLibs === 1){
+    madLibsExec(madLibsOne, madLibsOneStr, madLibsOneVals, madLibsOneTitle, randomMadLibs);
+} else if (randomMadLibs === 2) {
+    madLibsExec(madLibsTwo, madLibsTwoStr, madLibsTwoVals, madLibsTwoTitle, randomMadLibs);
+} else if (randomMadLibs === 3) {
+    madLibsExec(madLibsThree, madLibsThreeStr, madLibsThreeVals, madLibsThreeTitle, randomMadLibs);
+} else if (randomMadLibs === 4) {
+    madLibsExec(madLibsFour, madLibsFourStr, madLibsFourVals, madLibsFourTitle, randomMadLibs);
+}
 
 function madLibsExec(madLibsNum, madLibsStr, madLibsObj, madLibsTitle, madLibsRandomNum) {
     
+    $('#madLibsTitle').html(madLibsTitle);
 
     // Function to fill the Question Answer with all the inputs 
 
@@ -163,8 +459,57 @@ their ' + eval(madLibsStr + '13.value') + ' and shout, "' + eval(madLibsStr + '1
 
             console.log(madLibsNum);
         });
-    }
+    } else if (madLibsRandomNum === 2) {
+        $('#madLibsTwo18Save').on('click', function () {
+            console.log(madLibsObj);
 
-    
+            madLibsNum = 'It was a ' + eval(madLibsStr + '1.value') + ' day for Luke Skywalker. \
+He was learning how to be a real Jedi from his teacher, Master Yoda. Yoda is \
+only ' + eval(madLibsStr + '2.value') + ' feet tall and his skin is ' + eval(madLibsStr + '3.value') + '. \
+He is very wise and ' + eval(madLibsStr + '4.value') + '. He teaches Luke some ' + eval(madLibsStr + '5.value') + ' lessons: \
+To be a Jedi you must first be ' + eval(madLibsStr + '6.value') + '. You must also be strong. To build strength, balance \
+on your hands for ' + eval(madLibsStr + '7.value') + 'minutes and do ' + eval(madLibsStr + '8.value') + ' pushups. \
+This will make your muscles ' + eval(madLibsStr + '9.value') + '. Do not think ' + eval(madLibsStr + '10.value') + ' thoughts \
+and believe in yourself. A Jedi uses a ' + eval(madLibsStr + '11.value') + ' of great ' + eval(madLibsStr + '12.value') + ' called \
+a lightsaber. Luke\'s lightsaber is ' + eval(madLibsStr + '13.value') + '. He learns how to strike, block, \
+and ' + eval(madLibsStr + '14.value') + ' in a fight. He has to jump ' + eval(madLibsStr + '15.value') + ' and \
+run ' + eval(madLibsStr + '16.value') + '. Being a Jedi is not ' + eval(madLibsStr + '17.value') + '. It takes a lot of \
+practice. But, Luke us ready! He wants to ' + eval(madLibsStr + '18.value') + ' with the Dark Side and win!';
+
+            console.log(madLibsNum);
+        });
+    } else if (madLibsRandomNum === 3) {
+        $('#madLibsThree14Save').on('click', function () {
+            console.log(madLibsObj);
+
+            madLibsNum = 'There is no power in the universe quite like the Force. The Force is what gives a \
+Jedi his ' + eval(madLibsStr + '1.value') + 'power. It\'s an energy field created by all \
+' + eval(madLibsStr + '2.value') + eval(madLibsStr + '3.value') + '. It binds the ' + eval(madLibsStr + '4.value') + ' together. \
+The Force can be used for good or ' + eval(madLibsStr + '5.value') + '. The Dark Side is ' + eval(madLibsStr + '6.value') + 'and \
+the Side ' + eval(madLibsStr + '7.value') + ' is good! The Force ' + eval(madLibsStr + '8.value') + ' a Jedi strong. To use the Force \
+requires willpower, discipline and ' + eval(madLibsStr + '9.value') + '. A person who is strong with the Force can move objects through \
+the air with an outstretched ' + eval(madLibsStr + '10.value') + ' or, read the thoughts in another person\'s ' + eval(madLibsStr + '11.value') + '. \
+It takes a lot of energy to do this. Some people are born with a ' + eval(madLibsStr + '12.value') + 'connection with the Force. It\'s \
+a ' + eval(madLibsStr + '13.value') + 'compliment to say, "May the ' + eval(madLibsStr + '14.value') + ' be with you."';
+
+            console.log(madLibsNum);
+        });
+    } else if (madLibsRandomNum === 4) {
+        $('#madLibsFour12Save').on('click', function () {
+            console.log(madLibsObj);
+
+            madLibsNum = 'It\'s not easy being a princess! Especially, when you are a rebel. Princess Leia has a lot to do! \
+First, she puts on her make up, ' + eval(madLibsStr + '1.value') + ', and wraps her hair into a bun. She doesn\'t \
+wear a ' + eval(madLibsStr + '2.value') + 'dress! She has to be able to run and ' + eval(madLibsStr + '3.value') + '. Then, \
+she straps on ' + eval(madLibsStr + '4.value') + 'weapons. Her favourite one is a ' + eval(madLibsStr + '5.value') + ' blaster \
+pistol. It is like a gun, but instead of bullets, it shoots bright ' + eval(madLibsStr + '6.value') + '. In her training, she \
+doesn\'t learn to dance, paint, and ' + eval(madLibsStr + '7.value') + ' like most pricesses. Instead, she is taught how \
+to fight and ' + eval(madLibsStr + '8.value') + '. Leia is very brave. She knows how to swing a ' + eval(madLibsStr + '9.value') + ' and \
+dodge a strike. She can protect herself from ' + eval(madLibsStr + '10. value ') + ' villains. When her friends need help, she is \
+willing to ' + eval(madLibsStr + '11. value ') + 'to the rescue. She is a ' + eval(madLibsStr + '12.value') + ' princess.';
+
+            console.log(madLibsNum);
+        });
+    }
 
 }
